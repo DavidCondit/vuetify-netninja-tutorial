19ef94c66060bd324fd0c99cb64070a145cd941c
David Condit <david.condit@issvc.com>
2020-05-11 13:40:56 -0500

    Added several button examples with embedded icons. Showed how props can be used in place of classes. Shows how the dark prop makes text white.

--- todo-ninja/src/views/Home.vue
+++ todo-ninja/src/views/Home.vue
@@ -1,6 +1,30 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+
+    <v-btn class="pink white--text">click me</v-btn>
+    <v-btn dark depressed color="pink">click me</v-btn>
+    <v-btn text color="pink">click me</v-btn>
+
+    <v-btn depressed class="pink white--text">
+      <v-icon left>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>    
+    
+    <v-btn depressed small class="pink white--text">
+      <v-icon left small>mdi-email</v-icon>
+      <span>email me</span>
+    </v-btn>
+
+    <v-btn depressed large class="pink white--text">
+      <span>email me</span>
+      <v-icon right large>mdi-email</v-icon>
+    </v-btn>    
+
+    <v-btn fab depressed small dark color="purple">
+      <v-icon small>mdi-heart</v-icon>
+    </v-btn>    
+
   </div>
 </template>
 
