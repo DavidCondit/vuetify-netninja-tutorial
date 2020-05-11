83ea231ca0ba7447ccacc351526cb912522466db
David Condit <david.condit@issvc.com>
2020-05-11 17:28:39 -0500

    Learning how to use grid rows and columns.

--- todo-ninja/src/views/Dashboard.vue
+++ todo-ninja/src/views/Dashboard.vue
@@ -3,10 +3,49 @@
     <h1 class="subheading grey--text">Dashboard</h1>
     
     <v-container class="my-5">
-      content
+      
+      <v-row>
+        <v-col cols="12" md="6">
+          <v-btn outline block class="primary">1</v-btn>
+        </v-col>
+        <v-col cols="12" md="2">
+          <v-btn outline block class="primary">2</v-btn>
+        </v-col>   
+        <v-col cols="12" md="2">
+          <v-btn outline block class="primary">2</v-btn>
+        </v-col>   
+        <v-col cols="12" md="2">
+          <v-btn outline block class="primary">2</v-btn>
+        </v-col>                              
+      </v-row>
+
+      <v-row no-gutters>
+        <v-col cols="12" md="6">
+          <v-btn outline block class="primary">1</v-btn>
+        </v-col>
+        <v-col cols="12" md="2">
+          <v-btn outline block class="primary">2</v-btn>
+        </v-col>   
+        <v-col cols="12" md="2">
+          <v-btn outline block class="primary">2</v-btn>
+        </v-col>   
+        <v-col cols="12" md="2">
+          <v-btn outline block class="primary">2</v-btn>
+        </v-col>                              
+      </v-row>      
+
+      <v-row>
+        <v-col cols="4" md="3">
+          <v-btn outline block class="success">1</v-btn>
+        </v-col>
+        <v-col cols="4" md="3">
+          <v-btn outline block class="success">2</v-btn>
+        </v-col>        
+      </v-row>
+
     </v-container>
 
-    
+
   </div>
 </template>
 
