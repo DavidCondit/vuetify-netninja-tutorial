3664529dd2a541aa8a86d9cfa99140d90e02d814
David Condit <david.condit@issvc.com>
2020-05-11 16:29:36 -0500

    Added routes and views.

---
+++ todo-ninja/src/views/Team.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="team">
+    <h1>Team</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+export default {
+  name: 'Team',
+  components: {
+
+  }
+}
+</script>
