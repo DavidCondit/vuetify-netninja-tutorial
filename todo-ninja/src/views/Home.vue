b994c84e92d3eed0d6078432a940484a04e1db32
David Condit <david.condit@issvc.com>
2020-05-11 13:10:23 -0500

    Added text examples to Home.vue

--- todo-ninja/src/views/Home.vue
+++ todo-ninja/src/views/Home.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="home">
     <h1>Homepage</h1>
+    <p class="red white--text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Porro culpa nemo libero quo? Officia aspernatur explicabo ab totam quod corporis. Amet voluptatibus voluptate velit commodi sunt quod dolorum ea cumque.</p>
+    <p class="pink lighten-4 red--text text--darken-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores, doloribus beatae consequatur natus mollitia sed harum ratione non soluta voluptatibus? Pariatur obcaecati quo, quos nulla expedita quisquam eveniet consequuntur esse!</p>
+    <h1 class="display-4">Massive Display</h1>
+    <h4 class="display-1">Small Display</h4>
+    <p class="headline">This is a headline</p>
+    <p class="subheading font-weight-bold">This is a subheading</p>
+    <p class="caption">This is a caption</p>
   </div>
 </template>
 
