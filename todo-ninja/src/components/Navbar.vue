376e7e4e3c6deede8337c172b2165c3d752467c3
David Condit <david.condit@issvc.com>
2020-05-11 16:22:19 -0500

    Added a databound list to the navigation drawer element. Each databound item is then bound to the router to redirect the app to the specified route.

--- todo-ninja/src/components/Navbar.vue
+++ todo-ninja/src/components/Navbar.vue
@@ -14,7 +14,16 @@
         </v-app-bar>
 
         <v-navigation-drawer v-model="drawer" app temporary class="primary">
-            <p class="error">test</p>
+            <v-list>
+                <v-list-item v-for="link in links" :key="link.text" router :to="link.route">
+                    <v-list-item-action>
+                        <v-icon class="white--text">{{ link.icon }}</v-icon>
+                    </v-list-item-action>
+                    <v-list-item-content>
+                        <v-list-item-title class="white--text">{{ link.text }}</v-list-item-title>
+                    </v-list-item-content>
+                </v-list-item>
+            </v-list>
         </v-navigation-drawer>
 
     </nav>
@@ -25,7 +34,12 @@
 export default {
     data() {
         return {
-            drawer: false
+            drawer: false,
+            links: [
+                { icon: 'mdi-view-dashboard', text: 'Dashboard', route: '/' },
+                { icon: 'mdi-folder', text: 'My Projects', route: '/projects' },
+                { icon: 'mdi-face', text: 'Team', route: '/team' }
+            ]
         }
     }
 }
