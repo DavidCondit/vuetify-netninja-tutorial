717cc697dc638abfbbb7887a9966c4b6c65df8ac
David Condit <david.condit@issvc.com>
2020-05-12 12:23:05 -0500

    Added v-card with a row, which appears as a grid item. Data-binding to this as a template is the next step.

--- todo-ninja/src/views/Dashboard.vue
+++ todo-ninja/src/views/Dashboard.vue
@@ -2,63 +2,28 @@
   <div class="dashboard">
     <h1 class="subheading grey--text">Dashboard</h1>
     
-    <v-container class="my-5">
-      
+  <v-container class="my-5">
+    <v-card flat class="px-3">
       <v-row>
         <v-col cols="12" md="6">
-          <v-btn outline block class="primary">1</v-btn>
+          <div class="caption grey--text">Project Title</div>
+          <div>Create a new website</div>
         </v-col>
-        <v-col cols="12" md="2">
-          <v-btn outline block class="primary">2</v-btn>
-        </v-col>   
-        <v-col cols="12" md="2">
-          <v-btn outline block class="primary">2</v-btn>
-        </v-col>   
-        <v-col cols="12" md="2">
-          <v-btn outline block class="primary">2</v-btn>
-        </v-col>                              
+        <v-col>
+          <div class="caption grey--text">Person</div>
+          <div>The Net Ninja</div>
+        </v-col>
+        <v-col>
+          <div class="caption grey--text">Due Date</div>
+          <div>1st Jan 2021</div>
+        </v-col>
+        <v-col>
+          <div class="caption grey--text">Status</div>
+          <div>ongoing</div>
+        </v-col>
       </v-row>
-
-      <v-row class="mb-6" no-gutters>
-        <v-col cols="12" md="6">
-          <v-card class="pa-2" outlined tile>1</v-card>
-        </v-col>
-        <v-col cols="12" md="2">
-          <v-card class="pa-2" outlined tile>2</v-card>
-        </v-col>   
-        <v-col cols="12" md="2">
-          <v-card class="pa-2" outlined tile>2</v-card>
-        </v-col>   
-        <v-col cols="12" md="2">
-          <v-card class="pa-2" outlined tile>2</v-card>
-        </v-col>                              
-      </v-row>      
-
-      <v-row class="mb-6" no-gutters>
-        <v-col cols="6" md="3">
-          <v-card class="pa-2" outlined tile>1</v-card>
-        </v-col>
-        <v-col cols="2" md="3">
-          <v-card class="pa-2" outlined tile>2</v-card>
-        </v-col>   
-        <v-col cols="2" md="3">
-          <v-card class="pa-2" outlined tile>2</v-card>
-        </v-col>   
-        <v-col cols="2" md="3">
-          <v-card class="pa-2" outlined tile>2</v-card>
-        </v-col>                              
-      </v-row>        
-
-      <v-row>
-        <v-col cols="4" md="3">
-          <v-btn outline block class="success">1</v-btn>
-        </v-col>
-        <v-col cols="4" md="3">
-          <v-btn outline block class="success">2</v-btn>
-        </v-col>        
-      </v-row>
-
-    </v-container>
+  </v-card>
+  </v-container>
 
 
   </div>
